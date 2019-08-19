d45491f6fc178aec2e3154dd0fdc99f2b0e2a145
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-18 23:03:40 -0600

    sets up part of the modelController

--- CatFacts/CatFacts/ModelController/FactController.swift
+++ CatFacts/CatFacts/ModelController/FactController.swift
@@ -8,4 +8,39 @@
 
 import Foundation
 
-
+class FactController {
+    static func fetchFacts(completion: @escaping (String) -> Void) {
+        
+        
+        let baseURL = URL(string: "https://cat-fact.herokuapp.com")
+        
+        guard let url = baseURL else { completion(""); return }
+        
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        guard let requestURL = urlComponents?.url else { completion(""); return}
+        
+        print(requestURL)
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            
+            if let error = error {
+                print(error)
+                completion("")
+                return
+            }
+            
+            guard let data = data else { completion(""); return }
+            
+            do {
+                let topLevelObject = try JSONDecoder().decode(Fact.self, from: data)
+                completion(topLevelObject.text)
+            } catch {
+                print(error)
+                completion("")
+            }
+            
+        }.resume()
+        
+    }
+}
